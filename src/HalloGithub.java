0bc0d588b16cb87925125eae6992df18f47731c1
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-08-30 17:14:53 +0200

    Add Rest of the World feature

--- src/HalloGithub.java
+++ src/HalloGithub.java
@@ -1,5 +1,6 @@
 public class HalloGithub {
     public static void main(String[] args) {
         System.out.println("Hallo Github!");
+        System.out.println("Und Hallo Rest der Welt!‚");
     }
 }
